a94255feff7bcd7baf5d794ef9c5d79923f78ef5
vishwanath7 <ezhilvishwanath@gmail.com>
2019-10-13 18:11:22 -0400

    Feature: Updated python file

--- src/anagram.py
+++ src/anagram.py
@@ -5,3 +5,21 @@ l2=len(str2)
 a=[]
 b=[]
 count=0
+
+if(l1==l2):
+    for i in range(0,l1):
+        a.append(str1[i])
+        b.append(str2[i])
+    a.sort()
+    b.sort()
+    for i in range(0,l1):
+        if(a==b):
+            count+=1
+    if(count==l1):
+        print("Anagram")
+
+    else:
+        print("Not Anagram")
+
+else:
+    print("Not anagram")
